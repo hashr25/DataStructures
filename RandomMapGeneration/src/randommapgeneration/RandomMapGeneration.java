bef115c3cb7c6f70bbac6ba0dca346ff77538f74
Randy Hash <hashr25@mycu.concord.edu>
2015-11-30 12:06:53 -0500

    BlockMapGeneration & Drunkard's Walk
    Both implemented fully. Still working on mirrors and character
    movement. Still need to implement TinyKeep Algorithm and one other map
    generation.

--- RandomMapGeneration/src/randommapgeneration/RandomMapGeneration.java
+++ RandomMapGeneration/src/randommapgeneration/RandomMapGeneration.java
@@ -15,7 +15,26 @@ public class RandomMapGeneration {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int N = 50;
+        StdDraw.setXscale(-N, +N);
+        StdDraw.setYscale(-N, +N);
+        StdDraw.clear(StdDraw.GRAY);
+
+        int x = 0, y = 0;
+        int steps = 0;
+        while (Math.abs(x) < N && Math.abs(y) < N) {
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.filledSquare(x, y, 0.45);
+            double r = Math.random();
+            if      (r < 0.25) x--;
+            else if (r < 0.50) x++;
+            else if (r < 0.75) y--;
+            else if (r < 1.00) y++;
+            steps++;
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.filledSquare(x, y, .45);
+            StdDraw.show(40);
+        }
+        StdOut.println("Total steps = " + steps);
     }
-    
 }
